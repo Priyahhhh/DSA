19b0edabaddbaa2247a273a9784b58c5b6372d90
priyahhhh <ganeshanhemapriya16705>
2026-04-07 18:23:31 +0530

    check

---
+++ src/main/java/com/example/dsamain/DsaMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsamain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsaMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsaMainApplication.class, args);
+    }
+
+}
